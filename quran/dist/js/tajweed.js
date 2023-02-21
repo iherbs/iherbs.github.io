e3e61394dee4e035ca73605cbd6a74e4d57468a8
iherbs <herbsnext@gmail.com>
2023-02-22 00:41:29 +0700

    update

--- quran/dist/js/tajweed.js
+++ quran/dist/js/tajweed.js
@@ -1,160 +1,32 @@
-meta = [
-    {
-        'identifier': '[h',
-        'type': 'hamza-wasl',
-        'description': 'Hamzat ul Wasl',
-        'default_css_class': 'ham_wasl',
-        'html_color': '#AAAAAA'
-    },
-    {
-        'identifier': '[s',
-        'type': 'silent',
-        'description': 'Silent',
-        'default_css_class': 'slnt',
-        'html_color': '#AAAAAA'
-    },
-    {
-        'identifier': '[l',
-        'type': 'laam-shamsiyah',
-        'description': 'Lam Shamsiyyah',
-        'default_css_class': 'slnt',
-        'html_color': '#AAAAAA'
-    },
-    {
-        'identifier': '[n',
-        'type': 'madda-normal',
-        'description': 'Normal Prolongation: 2 Vowels',
-        'default_css_class': 'madda_normal',
-        'html_color': '#537FFF'
-    },
-    {
-        'identifier': '[p',
-        'type': 'madda-permissible',
-        'description': 'Permissible Prolongation: 2, 4, 6 Vowels',
-        'default_css_class': 'madda_permissible',
-        'html_color': '#4050FF'
-    },
-    {
-        'identifier': '[m',
-        'type': 'madda-necesssary',
-        'description': 'Necessary Prolongation: 6 Vowels',
-        'default_css_class': 'madda_necessary',
-        'html_color': '#000EBC'
-    },
-    {
-        'identifier': '[q',
-        'type': 'qalaqah',
-        'description': 'Qalaqah',
-        'default_css_class': 'qlq',
-        'html_color': '#DD0008'
-    },
-    {
-        'identifier': '[o',
-        'type': 'madda-obligatory',
-        'description': 'Obligatory Prolongation: 4-5 Vowels',
-        'default_css_class': 'madda_pbligatory',
-        'html_color': '#2144C1'
-    },
-    {
-        'identifier': '[c',
-        'type': 'ikhafa-shafawi',
-        'description': 'Ikhafa\' Shafawi - With Meem',
-        'default_css_class': 'ikhf_shfw',
-        'html_color': '#D500B7'
-    },
-    {
-        'identifier': '[f',
-        'type': 'ikhafa',
-        'description': 'Ikhafa\'',
-        'default_css_class': 'ikhf',
-        'html_color': '#9400A8'
-    },
-    {
-        'identifier': '[w',
-        'type': 'idgham-shafawi',
-        'description': 'Idgham Shafawi - With Meem',
-        'default_css_class': 'idghm_shfw',
-        'html_color': '#58B800'
-    },
-    {
-        'identifier': '[i',
-        'type': 'iqlab',
-        'description': 'Iqlab',
-        'default_css_class': 'iqlb',
-        'html_color': '#26BFFD'
-    },
-    {
-        'identifier': '[a',
-        'type': 'idgham-without-ghunnah',
-        'description': 'Idgham - With Ghunnah',
-        'default_css_class': 'idgh_ghn',
-        'html_color': '#169200'
-    },
-    {
-        'identifier': '[u',
-        'type': 'idgham-without-ghunnah',
-        'description': 'Idgham - Without Ghunnah',
-        'default_css_class': 'idgh_w_ghn',
-        'html_color': '#169200'
-    },
-    {
-        'identifier': '[d',
-        'type': 'idgham-mutajanisayn',
-        'description': 'Idgham - Mutajanisayn',
-        'default_css_class': 'idgh_mus',
-        'html_color': '#A1A1A1'
-    },
-    {
-        'identifier': '[b',
-        'type': 'idgham-mutaqaribayn',
-        'description': 'Idgham - Mutaqaribayn',
-        'default_css_class': 'idgh_mus',
-        'html_color': '#A1A1A1'
-    },
-    {
-        'identifier': '[g',
-        'type': 'ghunnah',
-        'description': 'Ghunnah: 2 Vowels',
-        'default_css_class': 'ghn',
-        'html_color': '#FF7E1E'
-    }
-];
+var arabicmatcher = /[؀-ٟ٪-ۯۺ-ۿ]+/;
 
-function parseArabic(text, show = "true", fixWebkit = true) {
-    if (fixWebkit) {
-        return this.webkitFix(this.closeParsing(this.parseTajweed(text, show)));
-    }
+var gunnahmatcher = /([ن|م]ّ|مِمَّ)/g; //
+var qalqalamatcher = /([قطبجد](ْ|ۡ|[^ه]?[^هىا]?[^هىا]$))/g; //
+var iqlabmmatcher = /([ۭۢ][ْۡاى]?[ۛۚۗۖۙۘ]? ?ب)/g; //
+var idhghammatcher = /([نًٌٍ][ْۡاى]?[ۛۚۗۖۙۘ]? [نميو]ّ)/g; //
+var idhghammatcherwihtoutgunnah = /([نًٌٍ][ْۡاى]?[ۛۚۗۖۙۘ]? [رل])/g; //
+var idhghammimimatcher = /([م][ْۛۚۗۖۙۘۡ]? م)/g; //
+var ikhfamatcher = /([نًٌٍ][ْۡاى]?[ۛۚۗۖۙۘ]? ?[صذثكجشقسدطزفتضظک])/g; //
+var ikhfasyamatcher = /([م][ْۡ]? ?ب)/g; //
+var madda = /(وْ|يْ)/g;
+var hamzawaslmatcher = /([ن|م]ّ)/g; //
 
-    return this.closeParsing(this.parseTajweed(text));
-}
+function parseArabic(text, show = "true") {
 
-function parseTajweed(text, show = "true") {
-    this.meta.forEach((meta) => {
-        let _re = new RegExp("(\\" + meta.identifier + ")", "ig");
-        text = text.replace(_re, `<tajweed class="${show == "true" ? meta.default_css_class : ''}" data-type="${meta.type}" data-description="${meta.description}" data-tajweed="`)
-    })
+    // &zwj;
+    // console.log(text);
+    // text = text.replace(gunnahmatcher, (text.substr((text.indexOf('$&') + 1), 2) == "ال" ? '&zwj;' : '') + '<tajweed class="gunnah">$&</tajweed>');
+    text = text.replace(gunnahmatcher, '<tajweed class="gunnah">$&</tajweed>');
+    text = text.replace(qalqalamatcher, '<tajweed class="qalqala">$&</tajweed>');
+    text = text.replace(iqlabmmatcher, '<tajweed class="iqlab">$&</tajweed>');
+    text = text.replace(idhghammatcher, '<tajweed class="idhgham">$&</tajweed>');
+    text = text.replace(idhghammatcherwihtoutgunnah, '<tajweed class="idhghamnoghunnah">$&</tajweed>');
+    text = text.replace(ikhfamatcher, '<tajweed class="ikhfa">$&</tajweed>');
+    text = text.replace(ikhfasyamatcher, '<tajweed class="ikhfasya">$&</tajweed>');
+    text = text.replace(idhghammimimatcher, '<tajweed class="idhghammimi">$&</tajweed>');
+    text = text.replace(madda, '<tajweed class="madda">$&</tajweed>');
+    // text = text.replace(hamzawaslmatcher, '<tajweed class="ham_wasl">$&</tajweed>');
 
-    return text;
-}
-
-function webkitFix(text) {
-    // Identify Tajweed tags, if there is not a space before or after, add &zwj;
-    // After
-    text = text.replace('/<\/tajweed>(\S)/', '&zwj;${0}')
-    // Before
-    text = text.replace('/(\S)<tajweed class="(.*?)" data-type="(.*?)" data-description="(.*?)" data-tajweed="(.*?)">(\S)/', '${1}<tajweed class="${2}" data-type="${3}" data-description="${4}" data-tajweed="${5}">&zwj;&zwj;${6}')
-
-    // Let's remove all joiners where not needed for an Alif and a Waw
-    text = text.replace(['ٱ&zwj;'], ['ٱ']);
-
-    return text;
-}
-
-function closeParsing(text) {
-    let re_1 = new RegExp("(\\[)", "ig");
-    text = text.replace(re_1, '">');
-    let re_2 = new RegExp("(\\])", "ig");
-    text = text.replace(re_2, '</tajweed>');
-
+    text = text.replaceAll('\u06dE', '<span class="smark">\u06dE</span>');
     return text;
 }
