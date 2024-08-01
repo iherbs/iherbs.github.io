a9326073b106583a96749c7ffe8c09fc2f700f21
iherbs <herbsnext@gmail.com>
2024-08-01 15:00:18 +0700

    update

--- quran/dist/js/tajweed.js
+++ quran/dist/js/tajweed.js
@@ -41,7 +41,7 @@ function parseArabic(text, show = "true") {
         text = text.replace(idhghammatcherwihtoutghunnah, '<tajweed class="idhghamnoghunnah">$&</tajweed>');
         // text = text.replace(ikhfamatcher, '<tajweed class="ikhfa">$&</tajweed>');
         text = text.replace(ikhfamatcher, function (match, capture) {
-            if (match == '\u0646\u0652\u0641') {
+            if (match == '\u0646\u0652\u0641' || match == '\u0646\u0652\u0643') {
                 return '<tajweed class="ikhfa">' + match + '</tajweed>';
             } else if (match.includes('\u0627 \u0642') || match.includes('\u064B\u0627')) {
                 return '<tajweed class="ikhfa">' + match.replace(' ', '</tajweed> <tajweed class="ikhfa">') + '</tajweed>';
