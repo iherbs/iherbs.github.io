025c47f9160c603091d8b3ad4efdd190d463d1d1
iherbs <herbsnext@gmail.com>
2025-05-04 08:12:13 +0700

    update

--- emojifarm/service-worker.js
+++ emojifarm/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'SW-001';
+const CACHE_NAME = 'EF-EMOJIFARM';
 const toCache = [
     '/',
     'assets/js/web.webmanifest',
@@ -6,39 +6,39 @@ const toCache = [
     'assets/img/icon.png',
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then(function(cache) {
-            return cache.addAll(toCache)
-        })
-        .then(self.skipWaiting())
+            .then(function (cache) {
+                return cache.addAll(toCache)
+            })
+            .then(self.skipWaiting())
     )
 })
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
     event.respondWith(
         fetch(event.request)
-        .catch(() => {
-            return caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.match(event.request)
+            .catch(() => {
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        return cache.match(event.request)
+                    })
             })
-        })
     )
 })
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
     event.waitUntil(
         caches.keys()
-        .then((keyList) => {
-            return Promise.all(keyList.map((key) => {
-                if (key !== CACHE_NAME) {
-                    console.log('[ServiceWorker] Hapus cache lama', key)
-                    return caches.delete(key)
-                }
-            }))
-        })
-        .then(() => self.clients.claim())
+            .then((keyList) => {
+                return Promise.all(keyList.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        console.log('[ServiceWorker] Hapus cache lama', key)
+                        return caches.delete(key)
+                    }
+                }))
+            })
+            .then(() => self.clients.claim())
     )
 })
